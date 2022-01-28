66099bad4c0343138d757787c7f5ede3a171ba67
chibuzc <chibuzc@yahoo.co.uk>
2022-01-28 05:56:01 +0100

    initial setup

---
+++ client/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Navbar } from './components';
+
+
+
+function App() {
+  return (
+    <div className='app'>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+
+      </div>
+      <div className="footer">
+
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
