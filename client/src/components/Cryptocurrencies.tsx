a41fec06a17446f28f681131787eb496e04fcf15
chibuzc <chibuzc@yahoo.co.uk>
2022-01-29 04:41:47 +0100

    implementing locak search

--- client/src/components/Cryptocurrencies.tsx
+++ client/src/components/Cryptocurrencies.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Card, Row, Col, Input } from 'antd'
 
@@ -14,16 +14,21 @@ interface CryptocurrenciesProps {
 const Cryptocurrencies = ({ simplified }: CryptocurrenciesProps) => {
   const count = simplified ? 10 : 100;
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count)
-  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins)
+  const [cryptos, setCryptos] = useState([])
   const [searchTerm, setSearchTerm] = useState('')
-  console.log(cryptos) 
 
-  if(isFetching) return <> Loading... </>
+  useEffect(() => {
+    const filteredData = cryptosList?.data?.coins?.filter((crypto: any) => (crypto?.name).toLowerCase().includes(searchTerm.toLowerCase()));
+    setCryptos(filteredData);
+  }, [cryptosList, searchTerm])
+
+  if (isFetching) return <> Loading... </>
 
   return (<>
-  <div>
-    <Input placeholder='Search Cryptocurrency' onChange={(e) => setSearchTerm(e.target.value)} />
-  </div>
+    {!simplified && (<div className='search-crypto'>
+      <Input placeholder='Search Cryptocurrency' onChange={(e) => setSearchTerm(e.target.value)} />
+    </div>)}
+
     <Row gutter={[32, 32]} className='crypto-card-container'>
       {cryptos?.map((crypto: any) =>
         <Col xs={24} sm={12} lg={6} className='crypto-card' key={crypto.uuid}>
